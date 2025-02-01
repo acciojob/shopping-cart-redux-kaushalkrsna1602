20efb5e4529c80e29d017c2dccc9236988eceff1
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-02-01 16:58:53 +0530

    c6

--- src/action.js
+++ src/action.js
@@ -24,3 +24,13 @@ export const addWishlist = (product) => ({
     type: actionTypes.addWishlist || "ADD_TO_WISHLIST",
     payload: product
 });
+
+
+export const actionTypes = {
+    addToCart: "ADD_TO_CART",
+    removeFromCart: "REMOVE_FROM_CART",
+    addItemQty: "ADD_ITEM_QTY",
+    removeItemQty: "REMOVE_ITEM_QTY",
+    addWishlist: "ADD_TO_WISHLIST",
+};
+
