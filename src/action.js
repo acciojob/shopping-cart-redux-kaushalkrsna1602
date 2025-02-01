8e854c05fc5e274b92ba299315393ad817178005
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-02-01 16:55:15 +0530

    c3

--- src/action.js
+++ src/action.js
@@ -1,27 +1,26 @@
-import {actionTypes} from './action'
+import { actionTypes } from './action.js'; // Ensure correct import path
 
 export const addToCart = (product) => ({
-    type: actionTypes.addToCart,
+    type: actionTypes.addToCart || "ADD_TO_CART", // Fallback in case of undefined
     payload: product,
 });
-  
+
 export const removeFromCart = (product) => ({
-    type: actionTypes.removeFromCart,
+    type: actionTypes.removeFromCart || "REMOVE_FROM_CART",
     payload: product
 });
 
 export const addItemQty = (product) => ({
-    type: actionTypes.addItemQty,
+    type: actionTypes.addItemQty || "ADD_ITEM_QTY",
     payload: product
 });
 
 export const removeItemQty = (product) => ({
-    type: actionTypes.removeItemQty,
+    type: actionTypes.removeItemQty || "REMOVE_ITEM_QTY",
     payload: product
 });
 
 export const addWishlist = (product) => ({
-    type: actionTypes.addWishlist,
+    type: actionTypes.addWishlist || "ADD_TO_WISHLIST",
     payload: product
 });
-  
